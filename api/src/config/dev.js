3b68aa1ed9e6287c7133bc3393d747ea4fa702ae
KINGWILLAN <148206247+KINGWILLAN@users.noreply.github.com>
2024-01-18 15:53:23 +0100

    added dev.js

---
+++ api/src/config/dev.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+// djdjjsdjhsdj
+
+export default {
+  PORT: process.env.PORT || 5000,
+  MONGO_URL: process.env.MONGO_URL,
+  SALT_ROUNDS: process.env.SALT_ROUNDS,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  AWS_SES_ACCESS_KEY: process.env.AWS_SES_ACCESS_KEY,
+  AWS_SES_SECRET_ACCESS_KEY: process.env.AWS_SES_SECRET_ACCESS_KEY,
+  AWS_SES_REGION: process.env.AWS_SES_REGION,
+  AWS_S3_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  AWS_S3_ACCESS_KEY: process.env.AWS_S3_ACESS_KEY,
+  AWS_S3_ARN: process.env.AWS_S3_ARN,
+  AWS_S3_REGION: process.env.AWS_S3_REGION,
+  AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  CLIENT_URL: process.env.CLIENT_URL,
+};
